7d93fa3eb6904fe6c8aa3bebfd38dfb3ed308392
Paulo Henrique <paulohenrique456moreira@gmail.com>
2024-04-21 22:16:25 -0300

    corrigindo bugs

--- js/script.js
+++ js/script.js
@@ -24,11 +24,6 @@ document.querySelector('.btnClose').addEventListener('click', function () {
 });
 
 
-// Input com Expressão Regular
-// document.getElementById('initCon').toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
-// document.getElementById('montCon').toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
-
-
 // Calculadora
 function calculateCompoundInterest () {
     let compInt, totalCompInt, invest, totalInvest, valTotal, calendar, per;
@@ -42,6 +37,11 @@ function calculateCompoundInterest () {
     let montCon = parseFloat(document.getElementById('montCon').value);
     let yldTime = parseInt(document.getElementById('yldTime').value);
     let intRate = parseFloat(document.getElementById('intRate').value);
+    let pieChart = document.getElementById('pieChart');
+    if (isNaN(initCon) || initCon < 0) {initCon = 0}
+    if (isNaN(montCon) || montCon < 0) {montCon = 0}
+    if (isNaN(yldTime) || yldTime < 0) {yldTime = 0}
+    if (isNaN(intRate)) {intRate = 0}
 
     if (yldTimeValue == months && intRateValue == monthly) {
         compInt = 0;
@@ -57,15 +57,13 @@ function calculateCompoundInterest () {
         per = "a.m";
         calendar = "Meses";
         updateResultValues(initCon, montCon, yldTime, intRate, totalInvest, totalCompInt, valTotal, calendar, per);
-
     } else if (yldTimeValue == years && intRateValue == yearly) {
         compInt = 0;
         totalCompInt = 0;
         invest = initCon + montCon;
         totalInvest = initCon + (montCon*(yldTime*12));
         for (let i = 0; i < (yldTime*12)-1; i++) {
-            // Arrumar formula de juros compostos
-            compInt = (invest * (intRate/12)) / 100;
+            compInt = (invest * ((((1 + (intRate/100))**(1/12)-1)*100)) / 100);
             totalCompInt += compInt;
             invest += compInt + montCon;
         }
@@ -73,15 +71,13 @@ function calculateCompoundInterest () {
         per = "a.a";
         calendar = "Anos";
         updateResultValues(initCon, montCon, yldTime, intRate, totalInvest, totalCompInt, valTotal, calendar, per);
-
     } else if (yldTimeValue == months && intRateValue == yearly) {
         compInt = 0;
         totalCompInt = 0;
         invest = initCon + montCon;
         totalInvest = initCon + (montCon*yldTime);
         for (let i = 0; i < yldTime-1; i++) {
-            // Arrumar formula de juros compostos
-            compInt = (invest * (intRate/12)) / 100;
+            compInt = (invest * ((((1 + (intRate/100))**(1/12)-1)*100)) / 100);
             totalCompInt += compInt;
             invest += compInt + montCon;
         }
@@ -89,14 +85,12 @@ function calculateCompoundInterest () {
         per = "a.a";
         calendar = "Meses";
         updateResultValues(initCon, montCon, yldTime, intRate, totalInvest, totalCompInt, valTotal, calendar, per);
-
     } else if (yldTimeValue == years && intRateValue == monthly) {
         compInt = 0;
         totalCompInt = 0;
         invest = initCon + montCon;
         totalInvest = initCon + (montCon*(yldTime*12));
         for (let i = 0; i < (yldTime*12)-1; i++) {
-            // Arrumar formula de juros compostos
             compInt = (invest * intRate) / 100;
             totalCompInt += compInt;
             invest += compInt + montCon;
@@ -106,7 +100,7 @@ function calculateCompoundInterest () {
         calendar = "Anos";
         updateResultValues(initCon, montCon, yldTime, intRate, totalInvest, totalCompInt, valTotal, calendar, per);
     }
-    function updateResultValues(initCon, montCon, yldTime, intRate, totalInvest, totalCompInt, valTotal) {
+    function updateResultValues(initCon, montCon, yldTime, intRate, totalInvest, totalCompInt, valTotal, calendar, per) {
         document.querySelector('.resultValInit').innerHTML = initCon.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
         document.querySelector('.resultValMont').innerHTML = montCon.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
         document.querySelector('.resultValYldTime').innerHTML = yldTime + " " + calendar;
@@ -115,94 +109,113 @@ function calculateCompoundInterest () {
         document.querySelector('.resultValRate').innerHTML = Number(totalCompInt).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
         document.querySelector('.resultValTotal').innerHTML = Number(valTotal).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
     }
+
+    let loopYear = 1;
+    for (let i = 0; i < yldTime-1; i++){loopYear += i}
+
+    let loopMonth;
+    for (let i = 0; i < yldTime-1; i++) {
+        loopMonth = "Janeiro";
+        loopMonth = "Fevereiro";
+        loopMonth = "Março";
+        loopMonth = "Abril";
+        loopMonth = "Maio";
+        loopMonth = "Junho";
+        loopMonth = "Julho";
+        loopMonth = "Agosto";
+        loopMonth = "Setembro";
+        loopMonth = "Outubro";
+        loopMonth = "Novembro";
+        loopMonth = "Dezembro";
+    }
+
+    // Gráficos de colunas (barra vertical)
+    if (yldTimeValue == years) {
+        google.charts.load('current', {'packages':['bar']});
+        google.charts.setOnLoadCallback(drawChart);
+        function drawChart () {
+            let data = google.visualization.arrayToDataTable([
+                ['Ano', 'Valor Inicial', 'Valor Investido', 'Valor em Juros'],
+                [loopYear, initCon, montCon*12, totalCompInt]
+            ]);
+            let options = {
+                bars: 'vertical',
+                vAxis: {format: 'short'},
+                width: 325,
+                height: 230,
+                colors: ['#B3A978', '#76ABAE', '#222831']
+            };
+            let chart = new google.charts.Bar(document.getElementById('columnChart'));
+            chart.draw(data, google.charts.Bar.convertOptions(options));
+        }
+    } else if (yldTimeValue == months) {
+        google.charts.load('current', {'packages':['bar']});
+        google.charts.setOnLoadCallback(drawChart);
+        function drawChart () {
+            let data = google.visualization.arrayToDataTable([
+                ['Mês', 'Valor Inicial', 'Valor Investido', 'Juros Compostos'],
+                // Criar um loop de repetição utilizando o resultado do tempo de rendimento
+                ['Janeiro', initCon, montCon, compInt],   
+                ['Fevereiro', initCon, montCon, compInt],
+                ['Março', initCon, montCon, compInt],
+                ['Abril', initCon, montCon, compInt],
+                ['Maio', initCon, montCon, compInt],
+                ['Junho', initCon, montCon, compInt],
+                ['Julho', initCon, montCon, compInt],
+                ['Agosto', initCon, montCon, compInt],
+                ['Setembro', initCon, montCon, compInt],
+                ['Outubro', initCon, montCon, compInt],
+                ['Novembro', initCon, montCon, compInt],
+                ['Dezembro', initCon, montCon, compInt]
+            ]);
+            let options = {
+                bars: 'vertical',
+                vAxis: {format: 'short'},
+                width: 325,
+                height: 230,
+                colors: ['#B3A978', '#76ABAE', '#222831']
+            };
+            let chart = new google.charts.Bar(document.getElementById('columnChart'));
+            chart.draw(data, google.charts.Bar.convertOptions(options));
+        }
+    }
+
+    // Gráfico de setores (redondo)
+    new Chart (pieChart, {
+        type: 'pie',
+        data: {
+            labels: [
+                'Valor Inicial',
+                'Valor em Juros',
+                'Valor Investido'
+            ],
+            datasets: [{
+                data: [initCon, totalCompInt.toFixed(2), totalInvest.toFixed(2)-initCon],
+                backgroundColor: [
+                    '#B3A978',
+                    '#76ABAE',
+                    '#222831'
+                ],
+            }]
+        },
+    });
 }
-document.getElementById('compute').addEventListener('click', calculateCompoundInterest);
 
-
-// Exibir seção dos resultados
-document.getElementById('compute').addEventListener('click', function () {
+// Resultado
+function openResult () {
     document.getElementById('result').style.display = 'flex';
     document.getElementById('resultContainer').style.display = 'flex';
-});
-document.querySelector('.newCalc').addEventListener('click', function () {
+    document.body.style.overflow = 'hidden';
+    calculateCompoundInterest();
+}
+document.getElementById('compute').addEventListener('click', openResult);
+document.addEventListener('keydown', function (event) {if (event.key === 'Enter') {openResult()}});
+function closeResult () {
     document.getElementById('result').style.display = 'none';
     document.getElementById('resultContainer').style.display = 'none';
-    document.querySelectorAll('#calculator input[type="number"]').forEach(function(input){input.value = null});
-});
-
-
-// Gráficos de colunas (barra vertical)
-// if (yldTimeValue == years) {} else if (yldTimeValue == months) {}
-    google.charts.load('current', {'packages':['bar']});
-    google.charts.setOnLoadCallback(drawChart);
-    function drawChart () {
-        let data = google.visualization.arrayToDataTable([
-            ['Ano', 'Valor Inicial', 'Valor Investido', 'Juros Compostos'],
-            // Valor Inicial = Valor Inicial || Valor Investido = Aporte Mensal*12 || Juros Compostos = Taxa de Juros (Anual) ou Taxa de Juros*12 (Mensal)
-            // Criar um loop de repetição utilizando o resultado do tempo de rendimento
-            ['2024', 1000, 400, 200], 
-        ]);
-        let options = {
-            bars: 'vertical',
-            vAxis: {format: 'short'},
-            width: 325,
-            height: 230,
-            colors: ['#76ABAE', '#31363E', '#EEE']
-        };
-        let chart = new google.charts.Bar(document.getElementById('columnChart'));
-        chart.draw(data, google.charts.Bar.convertOptions(options));
-    }
-
-
-    google.charts.load('current', {'packages':['bar']});
-    google.charts.setOnLoadCallback(drawChart);
-    function drawChart () {
-        let data = google.visualization.arrayToDataTable([
-          ['Mês', 'Valor Inicial', 'Valor Investido', 'Juros Compostos'],
-          // Valor Inicial = Valor Inicial || Valor Investido = Aporte Mensal || Juros Compostos = Taxa de Juros/12 (Anual) ou Taxa de Juros (Mensal)
-          // Criar um loop de repetição utilizando o resultado do tempo de rendimento
-          ['Janeiro', 1000, 400, 300],   
-          ['Fevereiro', 1170, 460, 300],
-          ['Março', 660, 1120, 300],
-          ['Abril', 660, 1120, 300],
-          ['Maio', 660, 1120, 300],
-          ['Junho', 660, 1120, 300],
-          ['Julho', 660, 1120, 300],
-          ['Agosto', 660, 1120, 300],
-          ['Setembro', 660, 1120, 300],
-          ['Outubro', 660, 1120, 300],
-          ['Novembro', 660, 1120, 300],
-          ['Dezembro', 1030, 540, 300]
-        ]);
-        let options = {
-          bars: 'vertical',
-          vAxis: {format: 'short'},
-          width: 325,
-          height: 230,
-          colors: ['#76ABAE', '#31363E', '#EEE']
-        };
-        let chart = new google.charts.Bar(document.getElementById('columnChart'));
-        chart.draw(data, google.charts.Bar.convertOptions(options));
-    }
-
-
-// Gráfico de setores (redondo)
-const pieChart = document.getElementById('pieChart');
-new Chart (pieChart, {
-    type: 'pie',
-    data: {
-        labels: [
-            'Valor Inicial',
-            'Valor em Juros',
-            'Valor total Investido'
-        ],
-        datasets: [{
-            data: [300, 50, 100], // Esses valores vao ser as variaveis do resultado "Valor Inicial", "Valor em Juros" e "Valor total Investido"
-            backgroundColor: [
-                '#76ABAE',
-                '#31363F',
-                '#EEE'
-            ],
-        }]
-    },
-});
+    document.body.style.overflow = 'visible';
+    document.body.style.overflowX = 'hidden';
+    window.location.reload();
+}
+document.querySelector('.newCalc').addEventListener('click', closeResult);
+document.addEventListener('keydown', function (event) { if (event.key === 'Escape') {closeResult()}});
